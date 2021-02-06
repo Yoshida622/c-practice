f77094796ec90daf820e9f82f390963785e96e2c
Yoshida <61640455+Yoshida622@users.noreply.github.com>
2021-02-06 13:22:29 +0900

    [Finish] 1.06

--- 1/1.06.cc
+++ 1/1.06.cc
@@ -32,3 +32,33 @@ int main() {
     cout << "xは10より小さくなくて、20より大きくもなくて、15でもない" << endl;
   }
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+ 
+  if (op == "+") {
+    cout << A + B << endl;
+  }
+  else if (op == "-") {
+    cout << A - B << endl;
+  }
+  else if (op == "*") {
+    cout << A * B << endl;
+  }
+  else if (op == "/") {
+    if (B == 0) {
+      cout << "error" << endl;
+    }
+    else {
+      cout << A / B << endl;
+    }
+  }
+  else {
+    cout << "error" << endl;
+  }
+}
