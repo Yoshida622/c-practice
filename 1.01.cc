2f0539f7379b5b2c3edfded91bb320d29a7da49c
Yoshida <61640455+Yoshida622@users.noreply.github.com>
2021-02-06 11:52:20 +0900

    [Finish] 1.01

---
+++ 1.01.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "Hello, world!" << endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "こんにちは世界" << endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "a";
+  cout << "b" << endl;
+  cout << "c" << "d" << endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "Hello, world!" << endl; //Hello, worldと表示
+  /*ここも
+  コメント*/
+}
+
+// 練習問題
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder!" << endl;
+}
