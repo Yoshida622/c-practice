fea89b7b5496c16258caab7cf25fa68d5e323aa9
Yoshida <61640455+Yoshida622@users.noreply.github.com>
2021-02-06 13:21:59 +0900

    [Finish] 1.06

---
+++ 1/1.06.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int x;
+  cin >> x;
+ 
+  if (x < 10) {
+    cout << "xは10より小さい" << endl;
+  }
+ 
+  cout << "終了" << endl;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int x;
+  cin >> x;
+ 
+  if (x < 10) {
+    cout << "xは10より小さい" << endl;
+  }
+  else if (x > 20) {
+    cout << "xは10より小さくなくて、20より大きい" << endl;
+  }
+  else if (x == 15) {
+    cout << "xは10より小さくなくて、20より大きくなくて、15である" << endl;
+  }
+  else {
+    cout << "xは10より小さくなくて、20より大きくもなくて、15でもない" << endl;
+  }
+}
